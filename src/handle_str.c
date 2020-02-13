59d66b752e57845f347aac688c05bffb34546335
Eric Lazo <ericlazo@philip.home>
2020-02-13 01:25:12 +0100

    something

--- src/handle_str.c
+++ src/handle_str.c
@@ -2,11 +2,31 @@
 
 #include "printf.h"
 
+	// eventually move this to another file
+	// just for char * for now
+char	*ft_salloc(size_t len, char fill)
+{
+	char	*ret;
+	int		i;
+
+	if (len < 1)		// other conditions ???
+		return (NULL);
+	if (!(ret = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	ret[len + 1] = '\0';
+	ft_memset(ret, fill, len);
+	return (ret);
+}	// string alloc
+
+
+
 	// eventually add spec struct to args...
 
 int			ft_handle_str(va_list ap, char **str, t_param *p)
 {
 	char	*tmp;
+	char	*pre;
+	char	*post;
 	int		len;
 
 	tmp = NULL;
@@ -18,15 +38,48 @@ int			ft_handle_str(va_list ap, char **str, t_param *p)
 		// do the char thing
 		*tmp = va_arg(ap, char);
 		len = 1;
-	}
+//			p->width > 1 ? p->width : 1;	// i think, there might be a
+	}										// special case for 0
 	else
 	{
 		// string
 		tmp = va_arg(ap, char*);
 		while (tmp[len])
 			++len;
+//		len = p->width > len ? p->width : len;
 	}
 	// now what ???
+
+	if (p->width > len)		// i think, but low key no idea need to test
+	{
+		len = p->width - len;		// use calloc ??? could actually be dope
+				// make my own function that allocates and fills but null 
+				// terminates
+		
+		if ((p->flag & 7) == 1)
+		{
+			// len spaces after string
+			post = ft_salloc(len, ' ');		//secure ???
+		}
+		else if ((p->flag & 1) == 1)
+		{
+			// len 0's before string
+			pre = ft_salloc(len, '0');
+		}
+		else
+		{
+			// len spaces before string
+			pre = ft_salloc(len, ' ');
+		}
+
+	}
+
+	if ((p->flag & 1) == 1)		// means there is a 0 ???
+								// if width and no -
+	if ((p->flag & 7) == 1)		// there is a -		// most important
+	if ((p->flag & 15) == 1)	// there is a space	// only applies if 
+								// width too small
+						// wait no, it's irrelevant ???
 
 	// precision is meaningless to strings
 
@@ -34,7 +87,9 @@ int			ft_handle_str(va_list ap, char **str, t_param *p)
 	// if yes, by how much
 	// is there a 0
 	// is there a -
-	
+
+
+	// 0's only if before not if - flag, otherwise spaces	
 
 
 	// use p->type which is the int, to do shit, a generic function that could be applied to all handles would be nice...
